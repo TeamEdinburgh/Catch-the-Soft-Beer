a0ee8ad463bfa319de73bdcaeea57212b049f9cd
Dimitvp <zeitgeistdimi@gmail.com>
2016-04-05 14:05:25 +0300

    New methods in Display

--- Catch-the-Soft-Beer/src/Display.java
+++ Catch-the-Soft-Beer/src/Display.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 
 
 public class Display {
+    private final int WIDTH = 730;
+    private final int HEIGHT = 730;
     private String name;
-    private int width;
-    private int height;
+
 
     private JFrame frame;
 
@@ -14,5 +16,11 @@ public class Display {
 
     private void init(String name){
         this.frame = new JFrame(name);
+        this.frame.setVisible(true);
+        this.frame.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+        this.frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.frame.setMaximumSize(new Dimension(WIDTH, HEIGHT));
+        this.frame.setFocusable(true);
+        this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
