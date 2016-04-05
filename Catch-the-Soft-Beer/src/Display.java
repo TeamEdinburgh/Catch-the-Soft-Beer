cb9725994633e495a90b12c41cee7f51abc5673b
Dimitvp <zeitgeistdimi@gmail.com>
2016-04-05 13:57:32 +0300

    Creating new methods in Display

--- Catch-the-Soft-Beer/src/Display.java
+++ Catch-the-Soft-Beer/src/Display.java
@@ -1,4 +1,18 @@
+import javax.swing.*;
+
 
 public class Display {
+    private String name;
+    private int width;
+    private int height;
 
+    private JFrame frame;
+
+    public Display(String name){
+        init(name);
+    }
+
+    private void init(String name){
+        this.frame = new JFrame(name);
+    }
 }
--- Catch-the-Soft-Beer/src/Game.java
+++ Catch-the-Soft-Beer/src/Game.java
@@ -1,6 +1,7 @@
+import javax.swing.*;
 
 public class Game {
     public static void main(String[] args) {
-
+        new Display("Catch-the-Soft-Beer!");
     }
 }
